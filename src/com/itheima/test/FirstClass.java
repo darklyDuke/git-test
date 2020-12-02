aefd88ee4a22de7b303fadf2a0f744d8aab50f6b
tiger <2323795143@qq.com>
2020-12-02 08:42:10 +0800

    本地工程添加到git

---
+++ src/com/itheima/test/FirstClass.java
@@ -0,0 +1,12 @@
+package com.itheima.test;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+        System.out.println("111111111111");
+    }
+}
